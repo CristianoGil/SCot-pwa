120f209e4481c16554d121d2492de9653d14b4d4
Vesmério António Xavier <66726762+vesmerioxavier@users.noreply.github.com>
2022-04-28 18:25:26 +0200

    Ad hoc/co direta (#5)
    * Login
    
    implementacao da UI - [Login]
    
    * add modal + fix dashboard
    
    feature
    
    * fix header, add [Modal & Propover]
    
    fix header, add [Modal & Propover]
    
    * implementacao de [Dark mode]
    
    implementacao de [Dark mode]
    
    * wip: setup/ui
    
    wip: setup/ui
    
    * init
    
    * wip: fix organization paths
    
    wip: fix organization paths

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { IonContent } from '@ionic/react';
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+
+
+
+  return (
+    <>
+      <IonContent color='#FCFCFC'>
+
+
+      </IonContent>
+    </>
+  );
+};
+
+export default Dashboard;
