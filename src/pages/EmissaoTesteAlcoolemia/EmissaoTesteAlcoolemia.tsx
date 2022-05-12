81242214d1b7735b31e7ff989e407270f01684f7
Vesmério António Xavier <66726762+vesmerioxavier@users.noreply.github.com>
2022-05-12 19:31:21 +0200

    wip: popoverAbrir [Acoes complementares]

--- src/pages/EmissaoTesteAlcoolemia/EmissaoTesteAlcoolemia.tsx
+++ src/pages/EmissaoTesteAlcoolemia/EmissaoTesteAlcoolemia.tsx
@@ -18,6 +18,7 @@ import DocumentoIdentificacao from '../../components/Contra-Ordenacoes/Component
 import InformacoesAdicionais from '../../components/Contra-Ordenacoes/Components/InformacoesAdicionais/InformacoesAdicionais';
 import LocalInfraccao from '../../components/Contra-Ordenacoes/Components/LocalInfraccao/LocalInfraccao';
 import DatePicker from '../../components/Combos/DatePicker';
+import NumeroDocumento from '../../components/NumeroDocumento/NumeroDocumento';
 
 const CoIndirecta: React.FC = () => {
 
@@ -281,37 +282,12 @@ const CoIndirecta: React.FC = () => {
                             {/*END: Alcoolímetro*/}
                         </IonCol>
                     </IonRow>
-
                     <IonRow>
                         <IonCol size-sm='12' size-md="12" size-lg="11">
                             {/*START: Número de Documento*/}
-
                             <IonCard className={'co-pagamento'}>
-                                <IonCardContent>
-                                    <IonGrid>
-                                        <IonRow>
-
-                                            <IonCol size-sm='12' size-md='10' size-lg='12'>
-                                                <p style={{ color: 'red' }}>Apenas deverá preencher este número caso não tenha sido possível imprimir o documento e o tenha registado manualmente em pré-impresso(Não será gerado o respectivo expediente).</p>
-                                            </IonCol>
-
-                                        </IonRow>
-                                        <IonRow>
-
-                                            <IonCol size-sm='12' size-md='10' size-lg='4'>
-
-                                                <IonItem>
-                                                    <IonLabel position="floating" itemType="text" placeholder="Número de Documento">Número de Documento</IonLabel>
-                                                    <IonInput></IonInput>
-                                                </IonItem>
-
-                                            </IonCol>
-
-                                        </IonRow>
-                                    </IonGrid>
-                                </IonCardContent>
+                                <NumeroDocumento />
                             </IonCard>
-
                             {/*END: Número de Documento*/}
                         </IonCol>
                     </IonRow>
