930d11ecd30ad2205c3605cebdef272aa17b11c7
Vesmério António Xavier <66726762+vesmerioxavier@users.noreply.github.com>
2022-05-13 10:34:41 +0200

    Hot fix/conflicts vesmerio (#16)
    * wip: init
    
    * wip: fixing menu issue
    
    * wip: fix layout Co-Directa [Intervenientes, Dados da infracao & dados complementares]
    
    * Update Dashboard.tsx
    
    * wip: add global state in modalMenu
    
    * wip: add [Registro de Apreensão de Documentos]
    
    * wip: add [Emissao de apresentacao de documentos]
    
    * wip: add EmissaoApreensaoVeiculo
    
    * fix: resolve issue related with menu
    
    * wip: resolve conflit
    
    * wip: add Emissão do Teste de Alcoolémia
    
    * wip: popoverAbrir [Acoes complementares]
    
    * merge to develop
    
    * wip: add google maps
    
    * Update Local.tsx
    
    Co-authored-by: Cristiano Joao <cristiano.joao@vm.co.mz>

--- src/components/Contra-Ordenacoes/Components/LocalInfraccao/LocalInfraccao.tsx
+++ src/components/Contra-Ordenacoes/Components/LocalInfraccao/LocalInfraccao.tsx
@@ -1,7 +1,58 @@
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonIcon, IonInput, IonItem, IonLabel, IonListHeader, IonRow, IonSelect, IonSelectOption } from "@ionic/react";
-import { search } from "ionicons/icons";
+import { search, location } from "ionicons/icons";
+import { GoogleMap } from '@capacitor/google-maps';
+import { Geolocation } from '@capacitor/geolocation';
+import { useEffect, useRef, useState } from "react";
 
 const LocalInfraccao: React.FC = () => {
+
+    const [coordenadas, setCoordenadas] = useState({ "latitude": -19.831996961440094, "longitude": 34.83581986832666 });
+
+    //const apiKey = 'AIzaSyAj5vVYw4tPa-GkA99WJRZ_NeiTcUI2688';
+    const apiKey = 'AIzaSyCYy0Et8LID7WqxJxJOwFJ4_ejnepbKxAM'; //chave temporária
+    const mapRef = useRef<HTMLElement>();
+    let newMap: GoogleMap;
+
+    async function createMap() {
+        if (!mapRef.current) return;
+
+        newMap = await GoogleMap.create({
+            id: 'my-cool-map',
+            element: mapRef.current,
+            apiKey: apiKey,//process.env.REACT_APP_YOUR_API_KEY_HERE as string,
+            config: {
+                center: {
+                    lat: -19.831996961440094,
+                    lng: 34.83581986832666
+                },
+                zoom: 8
+            }
+        });
+    }
+
+    const getCurrentPosition = async () => {
+        const coordinates = await Geolocation.getCurrentPosition();
+        setCoordenadas({ "latitude": coordinates.coords.latitude, "longitude": coordinates.coords.longitude });
+        // Move the map programmatically
+        await newMap.setCamera({
+            coordinate: {
+                lat: coordinates.coords.latitude,
+                lng: coordinates.coords.longitude
+            }
+        });
+        // Add a marker to the map
+        await newMap.addMarker({
+            coordinate: {
+                lat: coordinates.coords.latitude,
+                lng: coordinates.coords.longitude
+            }
+        });
+    };
+
+
+    useEffect(() => {
+        createMap();
+    }, []);
 
     return (
 
@@ -12,6 +63,27 @@ const LocalInfraccao: React.FC = () => {
 
             <IonCardContent>
                 <IonGrid>
+                    <IonRow>
+                        <IonCol size-sm="12" size-md="12" size-lg="12" style={{ marginTop: 16 }}>
+                            <div className="component-wrapper">
+                                <capacitor-google-map ref={mapRef} style={{
+                                    display: 'inline-block',
+                                    width: '100%',
+                                    height: 400
+                                }}></capacitor-google-map>
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol size-sm="12" size-md="12" size-lg="12" style={{ marginTop: 16 }}>
+                            <IonItem lines="none">
+                                <span className="ion-padding-end">Lat: {coordenadas.latitude}</span>
+                                <span>Lng: {coordenadas.longitude}</span>
+                            </IonItem>
+
+                            <IonButton onClick={getCurrentPosition}>USAR MINHA POSIÇÃO ATUAL  <IonIcon icon={location} slot='start' /></IonButton>
+                        </IonCol>
+                    </IonRow>
                     <IonRow>
                         <IonCol size-sm="9" size-md="8" size-lg="4" style={{ marginTop: 16 }}>
                             <IonItem>
--- src/pages/RI-Catalogo/Local/Local.tsx
+++ src/pages/RI-Catalogo/Local/Local.tsx
@@ -1,5 +1,9 @@
 import {
-    IonCol, IonContent, IonGrid, IonPage, IonRow,
+    IonAccordion,
+    IonAccordionGroup,
+    IonButton,
+    IonButtons,
+    IonCol, IonContent, IonGrid, IonInput, IonItem, IonItemGroup, IonLabel, IonList, IonPage, IonRow, IonSelect, IonSelectOption,
 } from '@ionic/react';
 import Menu from '../../../components/Menu/Menu';
 import React from 'react';
@@ -67,6 +71,161 @@ const Local: React.FC = () => {
                 </IonGrid>
 
                 <IonGrid className="localPartial">
+                    <IonRow style={{ marginTop: 20 }}>
+                        <IonCol size-sm='12' size-md='12' size-lg='11'>
+                            <IonAccordionGroup>
+                                <IonAccordion value="colors">
+                                    <IonItem slot="header">
+                                        <IonButton slot='start'>ADICIONAR LOCAL</IonButton>
+                                    </IonItem>
+
+                                    <IonList slot="content">
+                                        <IonItem>
+                                            <IonGrid>
+                                                <IonRow>
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Tipo Arruamento</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="TipoArruamento">Tipo Arruamento 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Tipo</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Tipo">Tipo 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Subtipo</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Subtipo">Subtipo 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+                                                </IonRow>
+
+                                                <IonRow>
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 9 }}>
+                                                        <IonItem>
+                                                            <IonLabel
+                                                                position="floating" itemType="text"
+                                                                placeholder='Designação'>Designação</IonLabel>
+                                                            <IonInput
+                                                                required={true}
+                                                                clearInput={true}
+                                                            ></IonInput>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>País</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="País">País 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Distrito</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Distrito">Distrito 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+                                                </IonRow>
+
+                                                <IonRow>
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Conselho</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Conselho">Conselho 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Fraguesia</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Fraguesia">Fraguesia 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 16 }}>
+                                                        <IonItem>
+                                                            <IonLabel>Localidade</IonLabel>
+                                                            <IonSelect interface="popover">
+                                                                <IonSelectOption value="Localidade">Localidade 1</IonSelectOption>
+                                                            </IonSelect>
+                                                        </IonItem>
+                                                    </IonCol>
+                                                </IonRow>
+
+                                                <IonRow>
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 9 }}>
+                                                        <IonItem>
+                                                            <IonLabel
+                                                                position="floating" itemType="text"
+                                                                placeholder='Arruamento'>Arruamento</IonLabel>
+                                                            <IonInput
+                                                                required={true}
+                                                                clearInput={true}
+                                                            ></IonInput>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 9 }}>
+                                                        <IonItem>
+                                                            <IonLabel
+                                                                position="floating" itemType="text"
+                                                                placeholder='Nº Polícia'>Nº Polícia</IonLabel>
+                                                            <IonInput
+                                                                required={true}
+                                                                clearInput={true}
+                                                            ></IonInput>
+                                                        </IonItem>
+                                                    </IonCol>
+
+                                                    <IonCol size-sm='12' size-md='10' size-lg='4' style={{ marginTop: 9 }}>
+                                                        <IonItem>
+                                                            <IonLabel
+                                                                position="floating" itemType="text"
+                                                                placeholder='Zona_Bairro'>Zona/Bairro</IonLabel>
+                                                            <IonInput
+                                                                required={true}
+                                                                clearInput={true}
+                                                            ></IonInput>
+                                                        </IonItem>
+                                                    </IonCol>
+                                                </IonRow>
+
+                                                <IonRow>
+                                                    <IonCol size-sm='12' size-md='12' size-lg='12' style={{ marginTop: 9 }}>
+                                                        <IonItemGroup>
+                                                            <IonButton slot='end' color='primary'>SALVAR</IonButton>
+                                                            <IonButton slot='end' color='danger'>CANCELAR</IonButton>
+                                                        </IonItemGroup>
+                                                    </IonCol>
+                                                </IonRow>
+                                            </IonGrid>
+                                        </IonItem>
+                                    </IonList>
+                                </IonAccordion>
+                            </IonAccordionGroup>
+                        </IonCol>
+                    </IonRow>
+
                     <IonRow>
                         <IonCol size-sm='12' size-md="12" size-lg="11">
                             {/*START: Local*/}
@@ -74,7 +233,7 @@ const Local: React.FC = () => {
                             {/*END: Local*/}
                         </IonCol>
                     </IonRow>
-                    <IonRow style={{marginTop:20}}>
+                    <IonRow style={{ marginTop: 20 }}>
                         <IonCol size-sm='12' size-md='12' size-lg='11'>
                             <DataTable
                                 columns={columns}
